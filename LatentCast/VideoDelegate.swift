ed8844443af5079db01b222d9a8df28073b5c49b
hayyaoe <hayya947@gmail.com>
2026-06-26 10:41:25 +0700

    refactor: isolate video delegate into backgorund process using actor

--- LatentCast/VideoDelegate.swift
+++ LatentCast/VideoDelegate.swift
@@ -9,7 +9,7 @@ import Foundation
 import AVFoundation
 
 // Decoupled from MainActor, runs entirely on background capture queues
-class VideoDelegate: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+actor VideoDelegate: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     private var frameCount = 0
     private var lastFPSUpdateTime = Date()
     private let onFPSUpdated: @Sendable (Double, Int) -> Void
@@ -19,17 +19,23 @@ class VideoDelegate: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
         super.init()
     }
     
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        frameCount += 1
-        let now = Date()
-        let timeInterval = now.timeIntervalSince(lastFPSUpdateTime)
-        
-        if timeInterval >= 1.0 {
-            let calculatedFPS = Double(frameCount) / timeInterval
-            onFPSUpdated(calculatedFPS, frameCount)
-            
-            frameCount = 0
-            lastFPSUpdateTime = now
+    nonisolated func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+            Task {
+                await self.updateFPS()
+            }
         }
-    }
+        
+        private func updateFPS() {
+            frameCount += 1
+            let now = Date()
+            let timeInterval = now.timeIntervalSince(lastFPSUpdateTime)
+            
+            if timeInterval >= 1.0 {
+                let calculatedFPS = Double(frameCount) / timeInterval
+                onFPSUpdated(calculatedFPS, frameCount)
+                
+                frameCount = 0
+                lastFPSUpdateTime = now
+            }
+        }
 }
